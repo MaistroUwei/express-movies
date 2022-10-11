b66ac106119e39bce67053efc8385c1f6ee31636
MaistroUwei <0208735@up.edu.mx>
2022-10-10 19:58:42 -0500

    ayuda por favor

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ firebase.initializeApp({
 
 //Creando base de datos y ina colección en esta
 const db  = firebase.firestore();
-const moviesDB = db.collection('movies');
 
 //Creando app de express
 const app = express();
@@ -31,13 +30,85 @@ app.get('/', (req, res) => {
 app.post('/create', async (req, res) => {
     try{
         const {body: movie } = req;
-        const resp = moviesDB.doc(1).set(movie);
-        res.send(resp);
+        const moviesDB = db.collection('movies');   
+        const { _path: { sengments } } = await moviesDB.add(movie);
+        const id = sengments[1];
+        res.send({
+            status: 200,
+            id
+        });
     }catch (error){
         res.send(error);
 
     }
 });
+
+//READ
+app.get('/get-movie/:id', async (req, res) => {
+    try {
+        const {params : { id }} = req;
+        const moviesDB = db.collection('movies').doc(id);
+        const { _fieldsProto : { time, author, name, rating }} = await moviesDB.get();
+        res.send({
+            status: 200,
+            time: time.stringValue,
+            author: author.stringValue,
+            name: name.stringValue,
+            rating: rating.stringValue
+        })
+    }catch(error){
+        res.send(error);
+    }
+})
+
+//DELETE
+app.delete('/delete-movie/:id', async (req, res) => {
+    try {
+        const { params : { id }} = req;
+        const movieDB = db.collection('movies').doc(id);
+        await movieDB.delete();
+        res.send({
+            status: 200
+        });
+    }catch(error){
+        res.send(error);
+    }
+});
+
+//UPDATE
+app.put('/update-movie', async (req, res) => {
+    try{
+        const {body: movie } = req;
+        const { id, time, author, name, rating } = movie;
+        const movieDB = db.collection('movies').doc(id);   
+        const resp = await movieDB.update({
+            name,
+            time,
+            rating,
+            author
+        });
+        res.send({
+            status: 200,
+            id
+        });
+    }catch (error){
+        res.send(error);
+
+    }
+});
+
+//GET MOVIES
+app.get('/get-movies', async (req, res) => {
+    try {
+        const moviesDB = await db.collection('movies').get();
+        const resp = moviesDB.docs.map(doc => doc.data());
+        res.send({
+            resp
+        })
+    }catch(error){
+        res.send(error);
+    }
+})
 
 //Diciendole a la app que awante por nuevas llamadas y duerma cuando nadie llegue
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
